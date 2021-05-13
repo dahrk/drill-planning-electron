82c44b76d6546874f25b901a4684fcb30b9eadbe
Tony Deng <9203995+dahrk@users.noreply.github.com>
2021-05-12 23:08:06 -0600

    enabling devtools

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
+  
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
